b5a045dfc29a1967286f9e47c50998a4cc2d7097
jonathanwarde <jonwarde@gmail.com>
2018-10-23 12:50:08 +0100

    Update Lecture3-Exercise3.js

--- Lecture3-Exercise3.js
+++ Lecture3-Exercise3.js
@@ -1,20 +1,34 @@
 // Exercise 3
 
+// An example of how chaining works
+
+// fluent APIs return the "this" object to allow a chain to continue
+
+// Only the getTotal method should return an actual value. Every other method returns the "this" object. 
+// As we are chaining we pass on the modified "this" object to the next method in the chain
+
 var obj = {
 	total: 0,
 	add: function(number){
-		return this.total + number;
+		this.total += number;
+		return this;
 	},
 	subtract: function(number){
-		return this.total - number;
+		this.total -= number;
+		return this;
 	},
 	increment: function(){
-		return this + 1;
+		this.total ++;
+		return this;
 	},
 	decrement: function(){
-		return this - 1;
+		this.total --;
+		return this;
 	},
 	getTotal: function(){
-		return this.total;
+		return obj.total;
 	}
 }
+
+chainedMethods1 = obj.increment().increment().subtract(5).getTotal();
+console.log('Result should be -3 :' +chainedMethods1);
